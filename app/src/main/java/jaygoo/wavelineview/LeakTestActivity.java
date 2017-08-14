5e6b497602c7447d33440e3fd9fa69cb97e284af
liubin-zzh <liubin@zhizihua.com>
2017-08-14 17:35:10 +0800

    首次上传 转自https://github.com/Jay-Goo/WaveLineView

---
+++ app/src/main/java/jaygoo/wavelineview/LeakTestActivity.java
@@ -0,0 +1,69 @@
+package jaygoo.wavelineview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import jaygoo.widget.wlv.WaveLineView;
+
+/**
+ * ================================================
+ * 作    者：JayGoo
+ * 版    本：
+ * 创建日期：2017/7/31
+ * 描    述:
+ * ================================================
+ */
+public class LeakTestActivity extends Activity{
+    private WaveLineView waveLineView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        waveLineView = (WaveLineView) findViewById(R.id.waveLineView);
+
+        findViewById(R.id.startBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                waveLineView.startAnim();
+            }
+        });
+
+        findViewById(R.id.stopBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                waveLineView.stopAnim();
+
+            }
+        });
+
+        findViewById(R.id.leakTestBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(LeakTestActivity.this,MainActivity.class));
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        waveLineView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        waveLineView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        waveLineView.release();
+    }
+}
---
+++ app/src/main/java/jaygoo/wavelineview/MainActivity.java
@@ -0,0 +1,70 @@
+package jaygoo.wavelineview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import jaygoo.widget.wlv.WaveLineView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WaveLineView waveLineView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        waveLineView = (WaveLineView) findViewById(R.id.waveLineView);
+
+        findViewById(R.id.startBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                waveLineView.startAnim();
+            }
+        });
+
+        findViewById(R.id.stopBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                waveLineView.stopAnim();
+
+            }
+        });
+
+        findViewById(R.id.leakTestBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(MainActivity.this,LeakTestActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+//            waveLineView.justDrawBackground();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        waveLineView.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        waveLineView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        waveLineView.release();
+    }
+}
